7e11bf7d41b57af7d6289036812a3326c2e34a2e
ZuxingGit <1245766137@qq.com>
2025-03-15 21:02:36 +1030

    change

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Start editing to see some MAGIC happen!</h2>
+      <h2>Start editing to see some MAGIC disappear!</h2>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
